20628b10d31a2b24c2eec38ec6a7cc04314b4727
Thetoicxdude <129590673+Thetoicxdude@users.noreply.github.com>
2024-09-19 20:38:11 +0800

    Update Membership.sol

--- .github/workflows/Membership.sol
+++ .github/workflows/Membership.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Membership {
-    mapping(address => uint) public userLevels;
+    mapping(address => uint256) public userLevels;
 
-    function setUserLevel(address _user, uint _level) external {
+    event UserRegistered(address indexed user, uint256 level);
+    
+    function registerUser(address _user, uint256 _level) external {
+        require(_level > 0, "Invalid level");
         userLevels[_user] = _level;
+        emit UserRegistered(_user, _level);
     }
 
-    function getUserLevel(address _user) external view returns (uint) {
+    function getUserLevel(address _user) external view returns (uint256) {
         return userLevels[_user];
     }
 }
