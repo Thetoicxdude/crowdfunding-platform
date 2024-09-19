b60ec25f61e42ed7feaa60b052746fd8b6735566
Thetoicxdude <129590673+Thetoicxdude@users.noreply.github.com>
2024-09-19 20:36:51 +0800

    Update Token.sol

--- .github/workflows/Token.sol
+++ .github/workflows/Token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PlatformToken is ERC20 {
     constructor() ERC20("PlatformToken", "PTK") {
-        _mint(msg.sender, 1000000 * 10 ** 18);  // 初始发行100万个代币
+        _mint(msg.sender, 1000000 * 10 ** 18);
     }
 }
