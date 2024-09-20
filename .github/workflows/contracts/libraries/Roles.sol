3b25911483843bedcd911df172eca474b079c29f
Thetoicxdude <129590673+Thetoicxdude@users.noreply.github.com>
2024-09-20 23:33:54 +0800

    Update and rename .github/workflows/Roles.sol to .github/workflows/contracts/libraries/Roles.sol

---
+++ .github/workflows/contracts/libraries/Roles.sol
@@ -0,0 +1,23 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Roles {
+    struct Role {
+        mapping(address => bool) bearer;
+    }
+
+    function add(Role storage role, address account) internal {
+        require(!has(role, account), "Roles: account already has role");
+        role.bearer[account] = true;
+    }
+
+    function remove(Role storage role, address account) internal {
+        require(has(role, account), "Roles: account does not have role");
+        role.bearer[account] = false;
+    }
+
+    function has(Role storage role, address account) internal view returns (bool) {
+        return role.bearer[account];
+    }
+}
